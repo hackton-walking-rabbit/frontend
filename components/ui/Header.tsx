689c860afb61b75ab97a1fcbad85b3a3c7210884
2fill <ichaeu@naver.com>
2025-08-24 16:48:38 +0900

    feat: mapPage 상단 헤더 추가

---
+++ components/ui/Header.tsx
@@ -0,0 +1,39 @@
+import { useFonts } from 'expo-font';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+interface HeaderProps {
+    title: string;
+}
+
+export const Header: React.FC<HeaderProps> = ({ title }) => {
+    // 폰트 로드
+    const [fontsLoaded] = useFonts({
+        Mynerve: require('../../assets/fonts/Mynerve-Regular.ttf'),
+    });
+
+    // 폰트 로드 전 빈 화면
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={[styles.title, { fontFamily: 'Mynerve', color: 'white' }]}>{title}</Text>
+        </View>
+    );
+    };
+
+    const styles = StyleSheet.create({
+    container: {
+        height: 110,
+        backgroundColor: '#338D29', 
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 50, 
+    },
+    title: {
+        fontSize: 20,
+        color: 'white',
+    },
+});
