0e126953d750cb4fd9d41e4fdbddde7783cd40ee
2fill <ichaeu@naver.com>
2025-08-24 16:31:09 +0900

    feat: 카카오맵 내 위치 표시 기능 추가

--- app/(tabs)/mapPage.tsx
+++ app/(tabs)/mapPage.tsx
@@ -1,9 +1,34 @@
 import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function MapPage() {
     const KAKAO_MAP_KEY = Constants.expoConfig?.extra?.kakaoMapKey;
+    const [coords, setCoords] = useState<{ latitude: number; longitude: number } | null>(null);
+
+    useEffect(() => {
+        (async () => {
+          // 위치 권한 요청
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                console.log('위치 권한이 필요합니다!');
+                return;
+            }
+        
+            // 현재 위치 가져오기
+            let location = await Location.getCurrentPositionAsync({});
+            setCoords({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+            });
+        })();
+    }, []);
+    
+    // 위치 못 받으면 동대문구 중심
+    const lat = coords?.latitude ?? 37.5744;
+    const lng = coords?.longitude ?? 127.0395;
 
     const html = `
         <!DOCTYPE html>
@@ -25,10 +50,15 @@ export default function MapPage() {
             <script>
                 var mapContainer = document.getElementById('map');
                 var options = {
-                center: new kakao.maps.LatLng(37.5665, 126.9780), // 서울
-                level: 3
+                    center: new kakao.maps.LatLng(${lat}, ${lng}),
+                    level: 3
                 };
                 var map = new kakao.maps.Map(mapContainer, options);
+
+                // 현재 위치 마커 표시
+                var markerPosition  = new kakao.maps.LatLng(${lat}, ${lng});
+                var marker = new kakao.maps.Marker({ position: markerPosition });
+                marker.setMap(map);
             </script>
             </body>
         </html>
