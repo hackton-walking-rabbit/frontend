16bf499e2548a37c33ee08489323e80f6260164b
2fill <ichaeu@naver.com>
2025-08-24 14:25:41 +0900

    feat: 카카오맵 추가

---
+++ app.config.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+
+export default {
+    expo: {
+        name: "my-app",
+        slug: "my-app",
+        extra: {
+        kakaoMapKey: process.env.KAKAO_MAP_KEY,
+        },
+    },
+};
--- app/(tabs)/mapPage.tsx
+++ app/(tabs)/mapPage.tsx
@@ -1,23 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function MapPage() {
+    const KAKAO_MAP_KEY = Constants.expoConfig?.extra?.kakaoMapKey;
+
+    const html = `
+        <!DOCTYPE html>
+        <html>
+            <head>
+            <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">
+            <style>
+                html, body, #map {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                }
+            </style>
+            <script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_MAP_KEY}"></script>
+            </head>
+            <body>
+            <div id="map"></div>
+            <script>
+                var mapContainer = document.getElementById('map');
+                var options = {
+                center: new kakao.maps.LatLng(37.5665, 126.9780), // 서울
+                level: 3
+                };
+                var map = new kakao.maps.Map(mapContainer, options);
+            </script>
+            </body>
+        </html>
+    `;
+
     return (
-        <View style={styles.titleContainer}>
-            <Text style={styles.title}>지도 페이지</Text>
+        <View style={{ flex: 1 }}>
+            <WebView originWhitelist={['*']} source={{ html }} style={{ flex: 1 }} />
         </View>
-
-    )
+    );
 }
-
-const styles = StyleSheet.create({
-    titleContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 8,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-    },
-});
