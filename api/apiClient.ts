33374ba53c0650d8a1cc8c75fda222f80cbcdf67
2fill <ichaeu@naver.com>
2025-08-30 05:58:17 +0900

    feat: cameraPage api 연결 1차 시도

--- api/apiClient.ts
+++ api/apiClient.ts
@@ -1,19 +1,20 @@
-import Constants from 'expo-constants';
-const BASE_URL = Constants.expoConfig?.extra?.apiBaseUrl;
+// import * as SecureStore from 'expo-secure-store';
+
+const BASE_URL = process.env.API_BASE_URL;
 
 export const apiFetch = async (url: string, options: any = {}) => {
-  const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0NDE5OTUyMjAzIiwiaWF0IjoxNzU2NDY2NTYwLCJleHAiOjE3NTY1NTI5NjB9.JO-F4boeZaABGUTuExqZp-mT2tXB8XiTsadlgkpT1_s';
+  const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0Mzg0NjUzMTI3IiwiaWF0IjoxNzU2NTAwOTczLCJleHAiOjE3NTY1ODczNzN9.qY-8kfwJj90uRN-IwO7I3wqFuv_44pWqKJjtlL-aDtE";
 
-  if (!token) {
-    throw new Error('accessToken이 없습니다. 로그인 후 다시 시도하세요.');
-  }
+  // headers가 FormData일 경우 Content-Type 제거
+  const isFormData = options.body instanceof FormData;
+  const headers = {
+    ...(token ? { Authorization: `Bearer ${token}` } : {}),
+    ...(isFormData ? {} : { 'Content-Type': 'application/json' }),
+    ...options.headers,
+  };
 
   return fetch(`${BASE_URL}${url}`, {
     ...options,
-    headers: {
-      'Authorization': token ? `Bearer ${token}` : '',
-      'Content-Type': 'application/json',
-      ...options.headers,
-    },
+    headers,
   });
 };
